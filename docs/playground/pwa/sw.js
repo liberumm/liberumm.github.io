2cfc616a1f02d87148cafd353d4cf497c1c0e42c
liberumm <62528460+liberumm@users.noreply.github.com>
2025-03-18 15:20:07 +0900

    ``` index.html を更新 ```

--- docs/playground/pwa/sw.js
+++ docs/playground/pwa/sw.js
@@ -1,23 +1,29 @@
+// sw.js
+
 // インストール時にキャッシュを作成
 self.addEventListener('install', event => {
-    event.waitUntil(
-      caches.open('v1').then(cache => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/manifest.json',
-          '/icon-192.png',
-          '/icon-512.png'
-        ]);
-      })
-    );
-  });
-  
-  // ネットワークから取得できない場合はキャッシュを返す
-  self.addEventListener('fetch', event => {
-    event.respondWith(
-      caches.match(event.request)
-        .then(response => response || fetch(event.request))
-    );
-  });
-  
+  event.waitUntil(
+    caches.open('v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icon-192.png',
+        '/icon-512.png'
+      ]);
+    })
+  );
+});
+
+// ネットワークフェッチ時、キャッシュからレスポンスを返す
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+// メッセージを受け取り、通知を表示する
+self.addEventListener('message', function (event) {
+  self.registration.showNotification(event.data);
+});
