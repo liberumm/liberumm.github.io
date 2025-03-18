b606beec20f541abd2c76aea479c1447f9ec5443
liberumm <62528460+liberumm@users.noreply.github.com>
2025-03-18 15:34:24 +0900

    ``` 変更なし ```

--- docs/playground/pwa/sw.js
+++ docs/playground/pwa/sw.js
@@ -5,7 +5,7 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('v1').then(cache => {
       return cache.addAll([
-        '.',
+        '/',
         'index.html',
         'manifest.json',
         'icon-192.png',
