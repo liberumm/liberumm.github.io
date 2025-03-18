1a429301b71ec5cd9d68619bbca3dbe4f9699297
liberumm <62528460+liberumm@users.noreply.github.com>
2025-03-18 15:32:23 +0900

    Service Worker のキャッシュ対象ファイルのパスを修正

--- docs/playground/pwa/sw.js
+++ docs/playground/pwa/sw.js
@@ -5,11 +5,11 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('v1').then(cache => {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/icon-192.png',
-        '/icon-512.png'
+        '.',
+        'index.html',
+        'manifest.json',
+        'icon-192.png',
+        'icon-512.png'
       ]);
     })
   );
